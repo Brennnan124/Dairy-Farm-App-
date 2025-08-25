f3e5af30f386e25142f6f64fda965012f532efed
Brennan <122364160+Brennnan124@users.noreply.github.com>
2025-08-25 18:25:17 +0300

    data_loader.py

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,7 +1,7 @@
-# dairy_farm_app/utils/data_loader.py
 import pandas as pd
 from datetime import date
 from firebase_utils import get_collection
+
 def load_table(table_name: str) -> pd.DataFrame:
     return get_collection(table_name)
 
